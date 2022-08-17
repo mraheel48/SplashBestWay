5c6bec1132c930411cfe79fa893a0549d1ea8d6b
mraheeldev <m.raheeldev@gmail.com>
2022-08-17 17:06:12 +0500

    update_color

--- app/src/main/java/com/example/splashbestway/SplashScreen.kt
+++ app/src/main/java/com/example/splashbestway/SplashScreen.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import com.example.splashbestway.databinding.ActivitySplashScreenBinding
@@ -19,7 +20,7 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.AppTheme)
+        setTheme(R.style.AppTheme2)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
