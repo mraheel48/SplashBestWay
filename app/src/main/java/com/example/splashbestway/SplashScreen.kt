f7ae68f01f840460bfa90a1c411d68d847df6fcb
mraheeldev <m.raheeldev@gmail.com>
2022-08-18 16:17:52 +0500

    done

--- app/src/main/java/com/example/splashbestway/SplashScreen.kt
+++ app/src/main/java/com/example/splashbestway/SplashScreen.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import com.example.splashbestway.databinding.ActivitySplashScreenBinding
